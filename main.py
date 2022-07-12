3b7dfcefc14b7e1ed59f3e8d9e390538b38bac43
SandbergM <marcus.a.sandberg@outlook.com>
2022-07-12 20:02:28 +0200

    no msg

--- main.py
+++ main.py
@@ -10,6 +10,5 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
